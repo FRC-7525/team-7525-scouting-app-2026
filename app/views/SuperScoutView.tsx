3e75e247a8ea0882440db55dc83e7cfc8b135460
sofiyapatra <patrasofiya07gmail.com>
2026-04-06 18:20:40 -0500

    adding defense ability slider

--- app/views/SuperScoutView.tsx
+++ app/views/SuperScoutView.tsx
@@ -12,6 +12,8 @@ function SuperScoutView() {
         <View style={styles.container}>
             <SectionTitle>Rank Drivers (1=best)</SectionTitle>
             <Slider min={1} max={3} step={1} onValueChange={(val) => updateDriverCitrusScale(val)} oldValue={getMatchData().then((data) => data["teleop"].driverCitrusScale)}/>
+            <SectionTitle>Defense Ability (1=best)</SectionTitle>
+            <Slider min={1} max={3} step={1} onValueChange={(val) => updateDefenseAbility(val)} oldValue={getMatchData().then((data) => data["teleop"].defenseAbility)}/>
         </View>
     );
 }
