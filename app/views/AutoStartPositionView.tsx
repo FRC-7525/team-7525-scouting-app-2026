04f05defab92f6a6abd288f78f3338a069ac5ad6
sofiyapatra <patrasofiya07gmail.com>
2026-04-10 02:58:32 -0500

    adding slot prop to autostartpositionview

--- app/views/AutoStartPositionView.tsx
+++ app/views/AutoStartPositionView.tsx
@@ -1,36 +1,30 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
 import RadioButton from '../components/RadioButton';
-import { getMatchData, updateLeftStart, updateStartPosition } from '../api/data';
-import { START_POSITION } from '../api/data_types';
-import SectionTitle from '../components/SectionTitle'
-import Switch from '../components/Switch';
+import { getMatchData, updateStartPosition } from '../api/data';
+import { OffenseRobotData, RobotSlot, START_POSITION } from '../api/data_types';
+import SectionTitle from '../components/SectionTitle';
 
+interface AutoStartPositionViewProps {
+    slot: RobotSlot;
+}
 
-function AutoStartPositionView () {
+function AutoStartPositionView({ slot }: AutoStartPositionViewProps) {
     return (
         <View style={styles.container}>
-                <SectionTitle> {'Starting Position:'}</SectionTitle>
-                <RadioButton data={["Scoring Table Side", "Center", "Audience Side"]}  
-                    onSelect={(option: string) => {
-                        updateStartPosition(option as START_POSITION);
-                    }}
-                    oldSelected={getMatchData().then((data) => data["autonomous"]["startPosition"])} />
-
-        </View>   
-    )
+            <SectionTitle>Starting Position:</SectionTitle>
+            <RadioButton
+                data={["Scoring Table Side", "Center", "Audience Side"]}
+                onSelect={(option: string) => updateStartPosition(slot, option as START_POSITION)}
+                oldSelected={getMatchData().then((data) => (data[slot] as OffenseRobotData).startPosition)} />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
-        // flexDirection: "row",
         paddingBottom: 10,
         alignItems: "center"
     },
-    side: {
-        flex: 1,
-        alignItems: 'center'
-    }
 });
 
 export default AutoStartPositionView;
